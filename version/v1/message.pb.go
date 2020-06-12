e68e6dca8f257b0fbecd8de4ac1299f36b91d183
Alex Shtin <alex@temporal.io>
2020-06-12 16:54:08 -0700

    Remove query consistency level from request (#46)

--- version/v1/message.pb.go
+++ version/v1/message.pb.go
@@ -46,24 +46,23 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
-// TODO: Rename to SupportedSDKVersions
-// SupportedClientVersions contains the support versions for SDK.
-type SupportedClientVersions struct {
+// SupportedSDKVersions contains the support versions for SDK.
+type SupportedSDKVersions struct {
 	GoSdk   string `protobuf:"bytes,1,opt,name=go_sdk,json=goSdk,proto3" json:"go_sdk,omitempty"`
 	JavaSdk string `protobuf:"bytes,2,opt,name=java_sdk,json=javaSdk,proto3" json:"java_sdk,omitempty"`
 }
 
-func (m *SupportedClientVersions) Reset()      { *m = SupportedClientVersions{} }
-func (*SupportedClientVersions) ProtoMessage() {}
-func (*SupportedClientVersions) Descriptor() ([]byte, []int) {
+func (m *SupportedSDKVersions) Reset()      { *m = SupportedSDKVersions{} }
+func (*SupportedSDKVersions) ProtoMessage() {}
+func (*SupportedSDKVersions) Descriptor() ([]byte, []int) {
 	return fileDescriptor_642dc4375f676645, []int{0}
 }
-func (m *SupportedClientVersions) XXX_Unmarshal(b []byte) error {
+func (m *SupportedSDKVersions) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
 }
-func (m *SupportedClientVersions) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+func (m *SupportedSDKVersions) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	if deterministic {
-		return xxx_messageInfo_SupportedClientVersions.Marshal(b, m, deterministic)
+		return xxx_messageInfo_SupportedSDKVersions.Marshal(b, m, deterministic)
 	} else {
 		b = b[:cap(b)]
 		n, err := m.MarshalToSizedBuffer(b)
@@ -73,26 +72,26 @@ func (m *SupportedClientVersions) XXX_Marshal(b []byte, deterministic bool) ([]b
 		return b[:n], nil
 	}
 }
-func (m *SupportedClientVersions) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_SupportedClientVersions.Merge(m, src)
+func (m *SupportedSDKVersions) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_SupportedSDKVersions.Merge(m, src)
 }
-func (m *SupportedClientVersions) XXX_Size() int {
+func (m *SupportedSDKVersions) XXX_Size() int {
 	return m.Size()
 }
-func (m *SupportedClientVersions) XXX_DiscardUnknown() {
-	xxx_messageInfo_SupportedClientVersions.DiscardUnknown(m)
+func (m *SupportedSDKVersions) XXX_DiscardUnknown() {
+	xxx_messageInfo_SupportedSDKVersions.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_SupportedClientVersions proto.InternalMessageInfo
+var xxx_messageInfo_SupportedSDKVersions proto.InternalMessageInfo
 
-func (m *SupportedClientVersions) GetGoSdk() string {
+func (m *SupportedSDKVersions) GetGoSdk() string {
 	if m != nil {
 		return m.GoSdk
 	}
 	return ""
 }
 
-func (m *SupportedClientVersions) GetJavaSdk() string {
+func (m *SupportedSDKVersions) GetJavaSdk() string {
 	if m != nil {
 		return m.JavaSdk
 	}
@@ -100,8 +99,7 @@ func (m *SupportedClientVersions) GetJavaSdk() string {
 }
 
 type WorkerVersionInfo struct {
-	// TODO: implementation
-	Impl           string `protobuf:"bytes,1,opt,name=impl,proto3" json:"impl,omitempty"`
+	Implementation string `protobuf:"bytes,1,opt,name=implementation,proto3" json:"implementation,omitempty"`
 	FeatureVersion string `protobuf:"bytes,2,opt,name=feature_version,json=featureVersion,proto3" json:"feature_version,omitempty"`
 }
 
@@ -137,9 +135,9 @@ func (m *WorkerVersionInfo) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_WorkerVersionInfo proto.InternalMessageInfo
 
-func (m *WorkerVersionInfo) GetImpl() string {
+func (m *WorkerVersionInfo) GetImplementation() string {
 	if m != nil {
-		return m.Impl
+		return m.Implementation
 	}
 	return ""
 }
@@ -152,42 +150,43 @@ func (m *WorkerVersionInfo) GetFeatureVersion() string {
 }
 
 func init() {
-	proto.RegisterType((*SupportedClientVersions)(nil), "temporal.version.v1.SupportedClientVersions")
+	proto.RegisterType((*SupportedSDKVersions)(nil), "temporal.version.v1.SupportedSDKVersions")
 	proto.RegisterType((*WorkerVersionInfo)(nil), "temporal.version.v1.WorkerVersionInfo")
 }
 
 func init() { proto.RegisterFile("temporal/version/v1/message.proto", fileDescriptor_642dc4375f676645) }
 
 var fileDescriptor_642dc4375f676645 = []byte{
-	// 286 bytes of a gzipped FileDescriptorProto
+	// 294 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x52, 0x2c, 0x49, 0xcd, 0x2d,
 	0xc8, 0x2f, 0x4a, 0xcc, 0xd1, 0x2f, 0x4b, 0x2d, 0x2a, 0xce, 0xcc, 0xcf, 0xd3, 0x2f, 0x33, 0xd4,
 	0xcf, 0x4d, 0x2d, 0x2e, 0x4e, 0x4c, 0x4f, 0xd5, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x12, 0x86,
-	0x29, 0xd1, 0x83, 0x2a, 0xd1, 0x2b, 0x33, 0x54, 0xf2, 0xe6, 0x12, 0x0f, 0x2e, 0x2d, 0x28, 0xc8,
-	0x2f, 0x2a, 0x49, 0x4d, 0x71, 0xce, 0xc9, 0x4c, 0xcd, 0x2b, 0x09, 0x83, 0x48, 0x16, 0x0b, 0x89,
-	0x72, 0xb1, 0xa5, 0xe7, 0xc7, 0x17, 0xa7, 0x64, 0x4b, 0x30, 0x2a, 0x30, 0x6a, 0x70, 0x06, 0xb1,
-	0xa6, 0xe7, 0x07, 0xa7, 0x64, 0x0b, 0x49, 0x72, 0x71, 0x64, 0x25, 0x96, 0x25, 0x82, 0x25, 0x98,
-	0xc0, 0x12, 0xec, 0x20, 0x7e, 0x70, 0x4a, 0xb6, 0x52, 0x00, 0x97, 0x60, 0x78, 0x7e, 0x51, 0x76,
-	0x6a, 0x11, 0xd4, 0x0c, 0xcf, 0xbc, 0xb4, 0x7c, 0x21, 0x21, 0x2e, 0x96, 0xcc, 0xdc, 0x82, 0x1c,
-	0xa8, 0x21, 0x60, 0xb6, 0x90, 0x3a, 0x17, 0x7f, 0x5a, 0x6a, 0x62, 0x49, 0x69, 0x51, 0x6a, 0x3c,
-	0xd4, 0x2d, 0x50, 0xa3, 0xf8, 0xa0, 0xc2, 0x50, 0x03, 0x9c, 0xa6, 0x30, 0x5e, 0x78, 0x28, 0xc7,
-	0x70, 0xe3, 0xa1, 0x1c, 0xc3, 0x87, 0x87, 0x72, 0x8c, 0x0d, 0x8f, 0xe4, 0x18, 0x57, 0x3c, 0x92,
-	0x63, 0x3c, 0xf1, 0x48, 0x8e, 0xf1, 0xc2, 0x23, 0x39, 0xc6, 0x07, 0x8f, 0xe4, 0x18, 0x5f, 0x3c,
-	0x92, 0x63, 0xf8, 0xf0, 0x48, 0x8e, 0x71, 0xc2, 0x63, 0x39, 0x86, 0x0b, 0x8f, 0xe5, 0x18, 0x6e,
-	0x3c, 0x96, 0x63, 0xe0, 0x92, 0xc9, 0xcc, 0xd7, 0x83, 0xfb, 0x16, 0xec, 0x7b, 0x24, 0x3f, 0x3b,
-	0xf1, 0xf8, 0x42, 0xc2, 0x25, 0x00, 0x24, 0x11, 0xc0, 0x18, 0x65, 0x90, 0x8e, 0xa4, 0x3a, 0x33,
-	0x5f, 0x1f, 0xc6, 0xd6, 0x05, 0xeb, 0x44, 0x0a, 0x50, 0x6b, 0x28, 0x33, 0x89, 0x0d, 0x2c, 0x63,
-	0x0c, 0x08, 0x00, 0x00, 0xff, 0xff, 0x52, 0xd8, 0xe1, 0x0c, 0x76, 0x01, 0x00, 0x00,
+	0x29, 0xd1, 0x83, 0x2a, 0xd1, 0x2b, 0x33, 0x54, 0xf2, 0xe0, 0x12, 0x09, 0x2e, 0x2d, 0x28, 0xc8,
+	0x2f, 0x2a, 0x49, 0x4d, 0x09, 0x76, 0xf1, 0x0e, 0x83, 0xc8, 0x14, 0x0b, 0x89, 0x72, 0xb1, 0xa5,
+	0xe7, 0xc7, 0x17, 0xa7, 0x64, 0x4b, 0x30, 0x2a, 0x30, 0x6a, 0x70, 0x06, 0xb1, 0xa6, 0xe7, 0x07,
+	0xa7, 0x64, 0x0b, 0x49, 0x72, 0x71, 0x64, 0x25, 0x96, 0x25, 0x82, 0x25, 0x98, 0xc0, 0x12, 0xec,
+	0x20, 0x7e, 0x70, 0x4a, 0xb6, 0x52, 0x0a, 0x97, 0x60, 0x78, 0x7e, 0x51, 0x76, 0x6a, 0x11, 0xd4,
+	0x0c, 0xcf, 0xbc, 0xb4, 0x7c, 0x21, 0x35, 0x2e, 0xbe, 0xcc, 0xdc, 0x82, 0x9c, 0xd4, 0xdc, 0xd4,
+	0xbc, 0x92, 0xc4, 0x92, 0xcc, 0xfc, 0x3c, 0xa8, 0x71, 0x68, 0xa2, 0x42, 0xea, 0x5c, 0xfc, 0x69,
+	0xa9, 0x89, 0x25, 0xa5, 0x45, 0xa9, 0xf1, 0x50, 0xc7, 0x41, 0x8d, 0xe7, 0x83, 0x0a, 0x43, 0x0d,
+	0x75, 0x9a, 0xc2, 0x78, 0xe1, 0xa1, 0x1c, 0xc3, 0x8d, 0x87, 0x72, 0x0c, 0x1f, 0x1e, 0xca, 0x31,
+	0x36, 0x3c, 0x92, 0x63, 0x5c, 0xf1, 0x48, 0x8e, 0xf1, 0xc4, 0x23, 0x39, 0xc6, 0x0b, 0x8f, 0xe4,
+	0x18, 0x1f, 0x3c, 0x92, 0x63, 0x7c, 0xf1, 0x48, 0x8e, 0xe1, 0xc3, 0x23, 0x39, 0xc6, 0x09, 0x8f,
+	0xe5, 0x18, 0x2e, 0x3c, 0x96, 0x63, 0xb8, 0xf1, 0x58, 0x8e, 0x81, 0x4b, 0x26, 0x33, 0x5f, 0x0f,
+	0xee, 0x7d, 0x70, 0x70, 0x20, 0x05, 0x82, 0x13, 0x8f, 0x2f, 0x24, 0xa0, 0x02, 0x40, 0x12, 0x01,
+	0x8c, 0x51, 0x06, 0xe9, 0x48, 0xaa, 0x33, 0xf3, 0xf5, 0x61, 0x6c, 0x5d, 0xb0, 0x4e, 0xa4, 0x10,
+	0xb6, 0x86, 0x32, 0x93, 0xd8, 0xc0, 0x32, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0x3c, 0x81,
+	0x82, 0xf6, 0x87, 0x01, 0x00, 0x00,
 }
 
-func (this *SupportedClientVersions) Equal(that interface{}) bool {
+func (this *SupportedSDKVersions) Equal(that interface{}) bool {
 	if that == nil {
 		return this == nil
 	}
 
-	that1, ok := that.(*SupportedClientVersions)
+	that1, ok := that.(*SupportedSDKVersions)
 	if !ok {
-		that2, ok := that.(SupportedClientVersions)
+		that2, ok := that.(SupportedSDKVersions)
 		if ok {
 			that1 = &that2
 		} else {
@@ -226,7 +225,7 @@ func (this *WorkerVersionInfo) Equal(that interface{}) bool {
 	} else if this == nil {
 		return false
 	}
-	if this.Impl != that1.Impl {
+	if this.Implementation != that1.Implementation {
 		return false
 	}
 	if this.FeatureVersion != that1.FeatureVersion {
@@ -234,12 +233,12 @@ func (this *WorkerVersionInfo) Equal(that interface{}) bool {
 	}
 	return true
 }
-func (this *SupportedClientVersions) GoString() string {
+func (this *SupportedSDKVersions) GoString() string {
 	if this == nil {
 		return "nil"
 	}
 	s := make([]string, 0, 6)
-	s = append(s, "&version.SupportedClientVersions{")
+	s = append(s, "&version.SupportedSDKVersions{")
 	s = append(s, "GoSdk: "+fmt.Sprintf("%#v", this.GoSdk)+",\n")
 	s = append(s, "JavaSdk: "+fmt.Sprintf("%#v", this.JavaSdk)+",\n")
 	s = append(s, "}")
@@ -251,7 +250,7 @@ func (this *WorkerVersionInfo) GoString() string {
 	}
 	s := make([]string, 0, 6)
 	s = append(s, "&version.WorkerVersionInfo{")
-	s = append(s, "Impl: "+fmt.Sprintf("%#v", this.Impl)+",\n")
+	s = append(s, "Implementation: "+fmt.Sprintf("%#v", this.Implementation)+",\n")
 	s = append(s, "FeatureVersion: "+fmt.Sprintf("%#v", this.FeatureVersion)+",\n")
 	s = append(s, "}")
 	return strings.Join(s, "")
@@ -264,7 +263,7 @@ func valueToGoStringMessage(v interface{}, typ string) string {
 	pv := reflect.Indirect(rv).Interface()
 	return fmt.Sprintf("func(v %v) *%v { return &v } ( %#v )", typ, typ, pv)
 }
-func (m *SupportedClientVersions) Marshal() (dAtA []byte, err error) {
+func (m *SupportedSDKVersions) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
 	dAtA = make([]byte, size)
 	n, err := m.MarshalToSizedBuffer(dAtA[:size])
@@ -274,12 +273,12 @@ func (m *SupportedClientVersions) Marshal() (dAtA []byte, err error) {
 	return dAtA[:n], nil
 }
 
-func (m *SupportedClientVersions) MarshalTo(dAtA []byte) (int, error) {
+func (m *SupportedSDKVersions) MarshalTo(dAtA []byte) (int, error) {
 	size := m.Size()
 	return m.MarshalToSizedBuffer(dAtA[:size])
 }
 
-func (m *SupportedClientVersions) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+func (m *SupportedSDKVersions) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	i := len(dAtA)
 	_ = i
 	var l int
@@ -328,10 +327,10 @@ func (m *WorkerVersionInfo) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i--
 		dAtA[i] = 0x12
 	}
-	if len(m.Impl) > 0 {
-		i -= len(m.Impl)
-		copy(dAtA[i:], m.Impl)
-		i = encodeVarintMessage(dAtA, i, uint64(len(m.Impl)))
+	if len(m.Implementation) > 0 {
+		i -= len(m.Implementation)
+		copy(dAtA[i:], m.Implementation)
+		i = encodeVarintMessage(dAtA, i, uint64(len(m.Implementation)))
 		i--
 		dAtA[i] = 0xa
 	}
@@ -349,7 +348,7 @@ func encodeVarintMessage(dAtA []byte, offset int, v uint64) int {
 	dAtA[offset] = uint8(v)
 	return base
 }
-func (m *SupportedClientVersions) Size() (n int) {
+func (m *SupportedSDKVersions) Size() (n int) {
 	if m == nil {
 		return 0
 	}
@@ -372,7 +371,7 @@ func (m *WorkerVersionInfo) Size() (n int) {
 	}
 	var l int
 	_ = l
-	l = len(m.Impl)
+	l = len(m.Implementation)
 	if l > 0 {
 		n += 1 + l + sovMessage(uint64(l))
 	}
@@ -389,11 +388,11 @@ func sovMessage(x uint64) (n int) {
 func sozMessage(x uint64) (n int) {
 	return sovMessage(uint64((x << 1) ^ uint64((int64(x) >> 63))))
 }
-func (this *SupportedClientVersions) String() string {
+func (this *SupportedSDKVersions) String() string {
 	if this == nil {
 		return "nil"
 	}
-	s := strings.Join([]string{`&SupportedClientVersions{`,
+	s := strings.Join([]string{`&SupportedSDKVersions{`,
 		`GoSdk:` + fmt.Sprintf("%v", this.GoSdk) + `,`,
 		`JavaSdk:` + fmt.Sprintf("%v", this.JavaSdk) + `,`,
 		`}`,
@@ -405,7 +404,7 @@ func (this *WorkerVersionInfo) String() string {
 		return "nil"
 	}
 	s := strings.Join([]string{`&WorkerVersionInfo{`,
-		`Impl:` + fmt.Sprintf("%v", this.Impl) + `,`,
+		`Implementation:` + fmt.Sprintf("%v", this.Implementation) + `,`,
 		`FeatureVersion:` + fmt.Sprintf("%v", this.FeatureVersion) + `,`,
 		`}`,
 	}, "")
@@ -419,7 +418,7 @@ func valueToStringMessage(v interface{}) string {
 	pv := reflect.Indirect(rv).Interface()
 	return fmt.Sprintf("*%v", pv)
 }
-func (m *SupportedClientVersions) Unmarshal(dAtA []byte) error {
+func (m *SupportedSDKVersions) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
 	iNdEx := 0
 	for iNdEx < l {
@@ -442,10 +441,10 @@ func (m *SupportedClientVersions) Unmarshal(dAtA []byte) error {
 		fieldNum := int32(wire >> 3)
 		wireType := int(wire & 0x7)
 		if wireType == 4 {
-			return fmt.Errorf("proto: SupportedClientVersions: wiretype end group for non-group")
+			return fmt.Errorf("proto: SupportedSDKVersions: wiretype end group for non-group")
 		}
 		if fieldNum <= 0 {
-			return fmt.Errorf("proto: SupportedClientVersions: illegal tag %d (wire type %d)", fieldNum, wire)
+			return fmt.Errorf("proto: SupportedSDKVersions: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		case 1:
@@ -567,7 +566,7 @@ func (m *WorkerVersionInfo) Unmarshal(dAtA []byte) error {
 		switch fieldNum {
 		case 1:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Impl", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field Implementation", wireType)
 			}
 			var stringLen uint64
 			for shift := uint(0); ; shift += 7 {
@@ -595,7 +594,7 @@ func (m *WorkerVersionInfo) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			m.Impl = string(dAtA[iNdEx:postIndex])
+			m.Implementation = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		case 2:
 			if wireType != 2 {
