f948449f09fcd3dc3623e7e98603676c08a4bf60
Shivs11 <shivam.saraf@temporal.io>
2024-05-28 19:03:24 -0400

    TaskQueueStats protos

--- taskqueue/v1/message.go-helpers.pb.go
+++ taskqueue/v1/message.go-helpers.pb.go
@@ -212,6 +212,43 @@ func (this *TaskQueueTypeInfo) Equal(that interface{}) bool {
 	return proto.Equal(this, that1)
 }
 
+// Marshal an object of type TaskQueueStats to the protobuf v3 wire format
+func (val *TaskQueueStats) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type TaskQueueStats from the protobuf v3 wire format
+func (val *TaskQueueStats) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *TaskQueueStats) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two TaskQueueStats values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *TaskQueueStats) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *TaskQueueStats
+	switch t := that.(type) {
+	case *TaskQueueStats:
+		that1 = t
+	case TaskQueueStats:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}
+
 // Marshal an object of type TaskQueueStatus to the protobuf v3 wire format
 func (val *TaskQueueStatus) Marshal() ([]byte, error) {
 	return proto.Marshal(val)
