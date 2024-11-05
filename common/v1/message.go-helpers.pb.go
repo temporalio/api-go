387c25fd10b7ad92a78999692d53d5f8c1b64c03
ShahabT <shahab.tajik@temporal.io>
2024-11-05 13:09:22 -0800

    update versioning-3 APIs

--- common/v1/message.go-helpers.pb.go
+++ common/v1/message.go-helpers.pb.go
@@ -508,6 +508,43 @@ func (this *WorkerVersionCapabilities) Equal(that interface{}) bool {
 	return proto.Equal(this, that1)
 }
 
+// Marshal an object of type WorkerDeployment to the protobuf v3 wire format
+func (val *WorkerDeployment) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type WorkerDeployment from the protobuf v3 wire format
+func (val *WorkerDeployment) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *WorkerDeployment) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two WorkerDeployment values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *WorkerDeployment) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *WorkerDeployment
+	switch t := that.(type) {
+	case *WorkerDeployment:
+		that1 = t
+	case WorkerDeployment:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}
+
 // Marshal an object of type ResetOptions to the protobuf v3 wire format
 func (val *ResetOptions) Marshal() ([]byte, error) {
 	return proto.Marshal(val)
