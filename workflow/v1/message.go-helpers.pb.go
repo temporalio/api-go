30496c1df659dc2a501054b36c305bf8dddb5e7e
ShahabT <shahab.tajik@temporal.io>
2025-01-12 18:05:12 -0800

    Replace Deployment with worker_build_id

--- workflow/v1/message.go-helpers.pb.go
+++ workflow/v1/message.go-helpers.pb.go
@@ -175,6 +175,43 @@ func (this *DeploymentTransition) Equal(that interface{}) bool {
 	return proto.Equal(this, that1)
 }
 
+// Marshal an object of type WorkerBuildTransition to the protobuf v3 wire format
+func (val *WorkerBuildTransition) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type WorkerBuildTransition from the protobuf v3 wire format
+func (val *WorkerBuildTransition) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *WorkerBuildTransition) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two WorkerBuildTransition values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *WorkerBuildTransition) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *WorkerBuildTransition
+	switch t := that.(type) {
+	case *WorkerBuildTransition:
+		that1 = t
+	case WorkerBuildTransition:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}
+
 // Marshal an object of type WorkflowExecutionConfig to the protobuf v3 wire format
 func (val *WorkflowExecutionConfig) Marshal() ([]byte, error) {
 	return proto.Marshal(val)
