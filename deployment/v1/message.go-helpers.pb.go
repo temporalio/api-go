fd875544aab87227520f81544a275a7d45d41906
Carly de Frondeville <carly.defrondeville@temporal.io>
2025-01-22 12:34:30 -0800

    make VersionDrainageInfo public

--- deployment/v1/message.go-helpers.pb.go
+++ deployment/v1/message.go-helpers.pb.go
@@ -286,6 +286,43 @@ func (this *WorkerDeploymentVersionInfo) Equal(that interface{}) bool {
 	return proto.Equal(this, that1)
 }
 
+// Marshal an object of type VersionDrainageInfo to the protobuf v3 wire format
+func (val *VersionDrainageInfo) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type VersionDrainageInfo from the protobuf v3 wire format
+func (val *VersionDrainageInfo) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *VersionDrainageInfo) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two VersionDrainageInfo values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *VersionDrainageInfo) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *VersionDrainageInfo
+	switch t := that.(type) {
+	case *VersionDrainageInfo:
+		that1 = t
+	case VersionDrainageInfo:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}
+
 // Marshal an object of type WorkerDeploymentInfo to the protobuf v3 wire format
 func (val *WorkerDeploymentInfo) Marshal() ([]byte, error) {
 	return proto.Marshal(val)
