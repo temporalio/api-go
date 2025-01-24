99329d5faeb800bd9a8a1b1cfcd469b4513da70c
Carly de Frondeville <carly.defrondeville@temporal.io>
2025-01-24 11:49:43 -0800

    pull latest

--- deployment/v1/message.go-helpers.pb.go
+++ deployment/v1/message.go-helpers.pb.go
@@ -359,3 +359,40 @@ func (this *WorkerDeploymentInfo) Equal(that interface{}) bool {
 
 	return proto.Equal(this, that1)
 }
+
+// Marshal an object of type RoutingInfo to the protobuf v3 wire format
+func (val *RoutingInfo) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type RoutingInfo from the protobuf v3 wire format
+func (val *RoutingInfo) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *RoutingInfo) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two RoutingInfo values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *RoutingInfo) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *RoutingInfo
+	switch t := that.(type) {
+	case *RoutingInfo:
+		that1 = t
+	case RoutingInfo:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}
