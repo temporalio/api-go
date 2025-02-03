1f14206b0af88d5b4647ccb319ea8cc79c1d6af2
Rodrigo Zhou <2068124+rodrigozhou@users.noreply.github.com>
2025-02-03 20:09:49 +0000

    Add OnConflictOptions to StartWorkflowExecutionRequest (#510)
    _**READ BEFORE MERGING:** All PRs require approval by both Server AND
    SDK teams before merging! This is why the number of required approvals
    is "2" and not "1"--two reviewers from the same team is NOT sufficient.
    If your PR is not approved by someone in BOTH teams, it may be summarily
    reverted._
    
    <!-- Describe what has changed in this PR -->
    **What changed?**
    Add `OnConflictOptions` to `StartWorkflowExecutionRequest`
    
    <!-- Tell your future self why have you made these changes -->
    **Why?**
    When the workflow id conflict policy is `USE_EXISTING`,
    `OnConflictOptions` add the ability to change the existing running
    workflow.
    
    <!-- Are there any breaking changes on binary or code level? -->
    **Breaking changes**
    
    <!-- If this breaks the Server, please provide the Server PR to merge
    right after this PR was merged. -->
    **Server PR**
    https://github.com/temporalio/temporal/pull/7080

--- workflow/v1/message.go-helpers.pb.go
+++ workflow/v1/message.go-helpers.pb.go
@@ -618,3 +618,40 @@ func (this *VersioningOverride) Equal(that interface{}) bool {
 
 	return proto.Equal(this, that1)
 }
+
+// Marshal an object of type OnConflictOptions to the protobuf v3 wire format
+func (val *OnConflictOptions) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type OnConflictOptions from the protobuf v3 wire format
+func (val *OnConflictOptions) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *OnConflictOptions) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two OnConflictOptions values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *OnConflictOptions) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *OnConflictOptions
+	switch t := that.(type) {
+	case *OnConflictOptions:
+		that1 = t
+	case OnConflictOptions:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}
