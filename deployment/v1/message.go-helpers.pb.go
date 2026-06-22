79810ba7096317dac3913fcdbe586e1e4cd0aad2
Muneeb Ahmad <muneeb.ahmad@temporal.io>
2026-06-22 22:35:39 +0000

    Add compute provider validation to worker deployment version summary (#810)
    **What changed?**
    Added `ComputeStatus` to the Worker Deployment API with a
    `ProviderValidationStatus` that tracks the result of the most recent
    connectivity check between Temporal and a customer's compute resource.
    An empty error message means validation passed; a non-empty message
    describes what failed.
    
    **Why?**
    Enables surfacing the connectivity health between Temporal and a
    customer's compute resource (e.g. Lambda) through the Worker Deployment
    API, so the UI can show ongoing validations status of compute configs
    without additional queries.
    
    **Breaking changes**
    None. All additions are new optional fields; existing clients are
    unaffected.
    
    **Server PR**
    https://github.com/temporalio/temporal/pull/10778

--- deployment/v1/message.go-helpers.pb.go
+++ deployment/v1/message.go-helpers.pb.go
@@ -264,6 +264,43 @@ func (this *VersionDrainageInfo) Equal(that interface{}) bool {
 	return proto.Equal(this, that1)
 }
 
+// Marshal an object of type ComputeStatus to the protobuf v3 wire format
+func (val *ComputeStatus) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type ComputeStatus from the protobuf v3 wire format
+func (val *ComputeStatus) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *ComputeStatus) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two ComputeStatus values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *ComputeStatus) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *ComputeStatus
+	switch t := that.(type) {
+	case *ComputeStatus:
+		that1 = t
+	case ComputeStatus:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}
+
 // Marshal an object of type WorkerDeploymentInfo to the protobuf v3 wire format
 func (val *WorkerDeploymentInfo) Marshal() ([]byte, error) {
 	return proto.Marshal(val)
